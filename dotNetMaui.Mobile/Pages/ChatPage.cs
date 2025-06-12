43bd16d486fc83e32930d6e07d1e3ed785c4411c
gonultasmf <gonultasmf@hotmail.com>
2025-06-13 00:49:21 +0300

    fix.

--- dotNetMaui.Mobile/Pages/ChatPage.cs
+++ dotNetMaui.Mobile/Pages/ChatPage.cs
@@ -37,7 +37,7 @@ public class ChatPage : ContentPage
 
                         new Label()
                         .Text(e => e.Getter(static (ChatPageViewModel x) => x.FriendInfo.UserName))
-                        .Margin(10,0,0,0)
+                        .Margin(10, 0, 0, 0)
                         .FontAttributes(Bold)
                         .FontSize(20)
                         .CenterVertical()
@@ -58,7 +58,7 @@ public class ChatPage : ContentPage
                         ),
 
                         new Button()
-                        .Margin(10,0,0,0)
+                        .Margin(10, 0, 0, 0)
                         .BackgroundColor("#152c39".ToColor())
                         .CornerRadius(45)
                         .SizeRequest(32)
@@ -68,76 +68,76 @@ public class ChatPage : ContentPage
                             .Glyph(IconFontHelper.Edit)
                             .Size(18)
                         )
-                    )
-                ),
+                    ),
 
-                new CollectionView()
-                .Row(1)
-                .Margin(0,10,0,10)
-                .ItemsSource(e => e.Getter(static (ChatPageViewModel viewModel) => viewModel.Messages))
-                .ItemsLayout(new LinearItemsLayout(ItemsLayoutOrientation.Vertical).ItemSpacing(10))
-                .ItemTemplate(() =>
-                    new VerticalStackLayout()
-                    .Assign(out var vLayout)
-                    .HorizontalOptions(e => e.Bindings(
-                            new Binding().Path("FromUserId"),
-                            new Binding().Path("ToUserId").Source(BindingContext as ChatPageViewModel)
-                        )
-                        .Converter(new FromUserIdToBackgroudColorConverter())
-                    )
-                    .Children(
-                        new Label()
-                        .Margin(5,0,5,0)
-                        .FontSize(11)
-                        .HorizontalOptions(e => e.Path("HorizontalOptions").Source(vLayout))
-                        .Text(e => e.Getter(static (Models.Message model) => model.SendDateTimeStr)),
-
-                        new Border()
-                        .Padding(10,6,10,6)
-                        .StrokeShape(new RoundRectangle().CornerRadius(10))
-                        .BackgroundColor(e => e.Bindings(
+                    new CollectionView()
+                    .Row(1)
+                    .Margin(0, 10, 0, 10)
+                    .ItemsSource(e => e.Getter(static (ChatPageViewModel viewModel) => viewModel.Messages))
+                    .ItemsLayout(new LinearItemsLayout(ItemsLayoutOrientation.Vertical).ItemSpacing(10))
+                    .ItemTemplate(() =>
+                        new VerticalStackLayout()
+                        .Assign(out var vLayout)
+                        .HorizontalOptions(e => e.Bindings(
                                 new Binding().Path("FromUserId"),
                                 new Binding().Path("ToUserId").Source(BindingContext as ChatPageViewModel)
                             )
                             .Converter(new FromUserIdToBackgroudColorConverter())
                         )
-                        .Content(
+                        .Children(
                             new Label()
-                            .Text(e => e.Getter(static (Models.Message model) => model.Content))
-                            .FontSize(12)
-                            .TextColor(Black)
-                        )
-                    )
-                ),
+                            .Margin(5, 0, 5, 0)
+                            .FontSize(11)
+                            .HorizontalOptions(e => e.Path("HorizontalOptions").Source(vLayout))
+                            .Text(e => e.Getter(static (Models.Message model) => model.SendDateTimeStr)),
 
-                new Grid()
-                .Row(2)
-                .ColumnDefinitions(e => e.Star().Absolute(40))
-                .Children(
-                    new Border()
-                    .Padding(10,0,10,0)
-                    .StrokeShape(new RoundRectangle().CornerRadius(20))
-                    .HeightRequest(36)
-                    .Stroke(Transparent)
-                    .Content(
-                        new Entry()
-                        .Placeholder("Aa")
-                        .Text(e => e.Getter(static (ChatPageViewModel model) => model.Message))
-                        .TextColor(Black)
-                        .CenterVertical()
+                            new Border()
+                            .Padding(10, 6, 10, 6)
+                            .StrokeShape(new RoundRectangle().CornerRadius(10))
+                            .BackgroundColor(e => e.Bindings(
+                                    new Binding().Path("FromUserId"),
+                                    new Binding().Path("ToUserId").Source(BindingContext as ChatPageViewModel)
+                                )
+                                .Converter(new FromUserIdToBackgroudColorConverter())
+                            )
+                            .Content(
+                                new Label()
+                                .Text(e => e.Getter(static (Models.Message model) => model.Content))
+                                .FontSize(12)
+                                .TextColor(Black)
+                            )
+                        )
                     ),
 
-                    new Button()
-                    .Column(1)
-                    .BackgroundColor("#152c39".ToColor())
-                    .Command(e => e.Getter(static (ChatPageViewModel model) => model.SendMessageCommand))
-                    .CornerRadius(45)
-                    .SizeRequest(32)
-                    .ImageSource(
-                        new FontImageSource()
-                        .FontFamily("IconFontTypes")
-                        .Glyph(IconFontHelper.Send)
-                        .Size(18)
+                    new Grid()
+                    .Row(2)
+                    .ColumnDefinitions(e => e.Star().Absolute(40))
+                    .Children(
+                        new Border()
+                        .Padding(10, 0, 10, 0)
+                        .StrokeShape(new RoundRectangle().CornerRadius(20))
+                        .HeightRequest(36)
+                        .Stroke(Transparent)
+                        .Content(
+                            new Entry()
+                            .Placeholder("Aa")
+                            .Text(e => e.Getter(static (ChatPageViewModel model) => model.Message))
+                            .TextColor(Black)
+                            .CenterVertical()
+                        ),
+
+                        new Button()
+                        .Column(1)
+                        .BackgroundColor("#152c39".ToColor())
+                        .Command(e => e.Getter(static (ChatPageViewModel model) => model.SendMessageCommand))
+                        .CornerRadius(45)
+                        .SizeRequest(32)
+                        .ImageSource(
+                            new FontImageSource()
+                            .FontFamily("IconFontTypes")
+                            .Glyph(IconFontHelper.Send)
+                            .Size(18)
+                        )
                     )
                 )
             )
@@ -145,7 +145,7 @@ public class ChatPage : ContentPage
     }
 
     private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
-	{
-		(this.BindingContext as ChatPageViewModel)!.Initialize();
-	}
+    {
+        (this.BindingContext as ChatPageViewModel)!.Initialize();
+    }
 }
